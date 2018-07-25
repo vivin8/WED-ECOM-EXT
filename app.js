c8cbfae2227a1c2f795c2b9af321cb3bf373a34c
Viktor Vinnik <viktor.vinnik@ikea.com>
2018-07-25 16:13:09 -0400

    checking local or Lambda 2.0

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ var path = require('path');
 var fs = require('fs');
 var os = require('os');
 
-var directory;
+// var directory;
 
-if (os.hostname().indexOf("local") > -1){
-  directory = 'Prod/';
+if (process.env['LAMBDA']){
+  app.set('base', '/Prod')
+  // var directory = 'Prod/';
 }
-else{
-  directory = '';
-}
+// else{
+//   directory = '';
+// }
 // var directory = '';
 // var directory = 'Prod/';
 var location = '/views/src/';
@@ -28,21 +29,21 @@ app.engine('html', ejs.renderFile);
 
 // index page 
 app.get('/', function(req, res) {
-	res.render(directory + 'pages/index.html');
+	res.render('pages/index.html');
 });
 
 // about page 
 app.get('/about', function(req, res) {
-	res.render(directory + 'pages/about.html');
+	res.render('pages/about.html');
 });
 
 app.use(express.static(__dirname + location));
 // 404
 app.get('*', function(req, res){
-	res.render(directory + 'pages/404.html');
+	res.render('pages/404.html');
 	});
 	
 app.listen(8080);
 console.log('8080 is the magic port');
-console.log(os.hostname().indexOf("local") > -1);
+console.log((process.env['LAMBDA']) !== undefined);
 module.exports = app;
